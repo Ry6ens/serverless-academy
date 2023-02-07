ce6be7bc760a731a529bb51a6ca10c5ff2479b04
Sergey Gulaga <ry6ens@gmail.com>
2023-02-07 16:01:55 +0200

    update first app

--- 00_cli_interactive_sort/app.js
+++ 00_cli_interactive_sort/app.js
@@ -8,6 +8,17 @@ rl.on('line', async input => {
     'Hello. Enter 10 words or digits deviding them in space: '
   );
 
+  const lengthAnswer = answerFirst
+    .trim()
+    .split(' ')
+    .filter(el => el !== '').length;
+
+  // Check on empty string or less two words or digits
+  if (lengthAnswer < 2) {
+    console.log('Please enter more than 2 words or digits');
+    return;
+  }
+
   const answerSecond = await rl.question(
     'What operation to do with words and numbers, namely: \n a. Sort words alphabetically \n b. Show numbers from lesser to greater \n c. Show numbers from bigger to smaller \n d. Display words in ascending order by number of letters in the word. \n e. Show only unique words \n f. Display only unique values from the set of words and numbers entered by the user \n g. To exit the program, the user need to enter exit, otherwise the program will repeat itself again and again, asking for new data and suggesting sorting \n \n Selected operation: '
   );
